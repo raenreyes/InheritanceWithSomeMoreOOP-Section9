692db81065796f0c80ec7c928a45a9934e8396e7
Raen Reyes <raen.reyes11@outlook.com>
2024-01-16 14:39:43 -0800

    Add project files.

---
+++ InheritanceWithSomeMoreOOP/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace InheritanceWithSomeMoreOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Udemy Section 9 Inheritance");
+        }
+    }
+}
